da00f1b1daa2a00e972607d0b682a2b0eb2c045a
spaceybread <spaceybread@gmail.com>
2024-09-11 10:59:20 -0500

    LIS DP

--- lis.py
+++ lis.py
@@ -1,5 +1,9 @@
-X = [3, 1, 4, 5, 2, 6]
+a = [3, 1, 4, 5, 2, 6, 2]
+n = len(a)
+best = [1] * n
 
-for k in range(len(X)):
-    for l in range(len(X)):
-        pass
+for i in range(1, n):
+    for p in range(0, i):
+        if a[i] > a[p]: best[i] = max(best[i], best[p] + 1)
+
+print(max(best))
