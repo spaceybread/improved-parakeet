b563102a4153b617dcc33942a4bcb35bef2eea7b
spaceybread <spaceybread@gmail.com>
2024-09-13 15:12:31 -0500

    Added comments

--- pollard.py
+++ pollard.py
@@ -1,16 +1,21 @@
+# absolute value function
 def abs(x):
     if x < 0: return -1 * x
     return x
 
+# polynomial used for Pollard's Rho Algorithm
 def g(x, n):
     return (x * x - 1) % n
 
+# Euclidean GCD
 def gcd(x, y):
     while(y): x, y = y, x % y
     return abs(x)
 
-N = 9
+# Number to be factored
+N = 547011651065197249
 
+# Init
 x, y, d = 2, 2, 1
 
 while d == 1:
